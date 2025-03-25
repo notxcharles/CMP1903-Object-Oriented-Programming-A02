dfa46640d5643bf0908eb92489d5fa068bf479cf
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-25 17:19:00 +0000

    Up to date with Dungeon Explorer

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DungeonExplorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("charles", 250);
+            Game game = new Game("Room Game", 5, player);
+            game.Start();
+            Console.ReadKey();
+        }
+    }
+}
