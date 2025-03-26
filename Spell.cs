36214333691852cd024801b401b3d87e87f1c3e9
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-26 23:14:05 +0000

    Class Spell extends Item

---
+++ Spell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    public class Spell : Item
+    {
+        private int _healAmount;
+        public Spell(string name) : base(name)
+        {
+
+        }
+        public int HealAmount
+        {
+            get { return _healAmount; }
+        }
+    }
+}
