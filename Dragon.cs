616b7ff5aac9d574f9c66e9ac2b0cd98c6a642af
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-27 12:06:45 +0000

    Dragon Class
    Dragon class features an override method of DisplayAttack()

---
+++ Dragon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    public class Dragon : Monster
+    {
+        public Dragon(string name, int health, int averageAttackDamage) : base(name, health, averageAttackDamage)
+        {
+
+        }
+
+        public override void DisplayAttack(int damage)
+        {
+            Console.WriteLine($"The Dragon breathes fire! It dealt {damage}!");
+        }
+    }
+}
