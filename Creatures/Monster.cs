667f4bca64279eb73a5559fc5eb318a59a29d91c
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-27 14:06:34 +0000

    Monster Difficulty
    Monster difficulty affects the damage that the monster does (difficulty is a coefficient)
    The difficulty can be set, otherwise it is randomly calculated for a value from 0.6 to 1.4

--- Creatures/Monster.cs
+++ Creatures/Monster.cs
@@ -41,6 +41,8 @@ namespace DungeonExplorer
             "Jabba the Hutt",
         };
         private Weapon _weapon;
+        private float _difficulty;
+        //TODO: Update documentation
         /// <summary>
         /// Class <c>Monster</c>'s constructor
         /// </summary>
@@ -53,24 +55,45 @@ namespace DungeonExplorer
             Debug.Assert(name != null, "Error: name does not exist");
             Testing.TestForPositiveInteger(health);
             _weapon = weapon;
+            _difficulty = CalculateRandomDifficulty();
         }
+        //TODO: Update documentation
         /// <summary>
         /// Class <c>Monster</c>'s constructor
         /// </summary>
         /// <param name="health">The maximum health of the monster</param>
         /// <param name="averageAttack">The average attack value that the monster does</param>
-        public Monster(int health, Weapon weapon) : base(health)
+        public Monster(int health, float difficulty, Weapon weapon) : base(health)
         {
             Name = CreateMonsterName();
             Testing.TestForPositiveInteger(health);
+            _difficulty = difficulty;
             _weapon = weapon;
         }
-
+        //TODO: Documentation
+        public Monster(int health, Weapon weapon) : base(health)
+        {
+            _weapon = weapon;
+            _difficulty = CalculateRandomDifficulty();
+        }
         // TODO: Documentation
         public Weapon Weapon
         {
             get { return _weapon; }
             protected set { _weapon = value; }
+        }
+        // TODO: documentation
+        public float Difficulty
+        {
+            get { return _difficulty; }
+            protected set { _difficulty = value; }
+        }
+        // TODO: Documentation
+        private float CalculateRandomDifficulty()
+        {
+            float randomValue = _random.Next(60, 140);
+            float randomDifficulty = randomValue/100;
+            return randomDifficulty;
         }
         /// <summary>
         /// From <c>Monster._monsterNames</c>, randomly select a name for the monster
@@ -87,11 +110,12 @@ namespace DungeonExplorer
         /// <returns><c>Monster._currentEquippedWeapon.GetAttackDamage()</c></returns>
         public int GetAttackDamage()
         {
-            return _weapon.GetAttackDamage();
+            return (int)(_weapon.GetAttackDamage()*_difficulty);
         }
         //TODO: Documentation Strings
         public virtual void DisplayAttack(int damage)
         {
+            Console.WriteLine($"DEBUG: {this.GetType().Name} has {_difficulty} difficulty");
             Console.WriteLine($"The {this.GetType().Name}, {Name} dealt {damage} damage");
         }
     }
