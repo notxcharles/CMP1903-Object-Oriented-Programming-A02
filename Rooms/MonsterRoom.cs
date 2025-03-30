114e31ef869737f5e16dc2c2ccaa5c3972125a55
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-30 17:26:22 +0100

    Create MonsterRoom

--- Rooms/MonsterRoom.cs
+++ Rooms/MonsterRoom.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DungeonExplorer.Rooms
 {
-    public class MonsterRoom
+    public class MonsterRoom : Room
     {
+        private Monster _monsterInTheRoom;
+        public MonsterRoom(Monster monster, Weapon weaponInTheRoom, Spell spellInTheRoom, Hint hint): base(weaponInTheRoom,  spellInTheRoom, hint)
+        {
+            _monsterInTheRoom = monster;
+        }
+        // TODO: Documentation
+        public bool MonsterIsAlive
+        {
+            get { return _monsterInTheRoom != null && _monsterInTheRoom.Health > 0; }
+        }
+        // TODO: Documentation
+        public Monster Monster
+        {
+            get { return _monsterInTheRoom; }
+        }
+        public void MonsterDefeated()
+        {
+            Debug.Assert(_monsterInTheRoom.Health <= 0, "Monster should have 0 or less health");
+            _monsterInTheRoom = null;
+            return;
+        }
     }
 }
