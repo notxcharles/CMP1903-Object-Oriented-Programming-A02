aa95512d114c8dba23f91fc616e2629d4ba5ea3c
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-31 19:03:58 +0100

    Basic Inventory and MaxLength

--- Inventory.cs
+++ Inventory.cs
@@ -8,9 +8,19 @@ namespace DungeonExplorer
 {
     public class Inventory
     {
-        public Inventory()
+        private List<Item> _inventory;
+        private int _maxLength;
+        public Inventory(int maxLength)
         {
+            _maxLength = maxLength;
+        }
 
+        public void Add(Item item)
+        {
+            if (_inventory.Count >= _maxLength)
+            {
+                Console.WriteLine("Your inventory is full, if you wish to add more items to your inventory then you must discard some items");
+            }
         }
     }
 }
