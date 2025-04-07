89d0ebc8bda52a46d648db0e4cdebf306a5a82ca
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-04-07 19:18:26 +0100

    UnitTests class

---
+++ UnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DungeonExplorer
+{
+    [TestClass]
+    public class UnitTests
+    {
+        private Game _game;
+        private Player _player;
+        public UnitTests() 
+        {
+            _player = new Player("TestingPlayer", 500, 8);
+            _game = new Game("TestingGame", _player);
+        }
+
+    }
+}
